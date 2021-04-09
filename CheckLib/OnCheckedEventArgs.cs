4a58c4caefe3d3e2d3c28282c2edf532fc95d7a9
MRGRD56 <kirgol85@gmail.com>
2021-04-09 09:25:07 +0500

    v1.0.3

---
+++ CheckLib/OnCheckedEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CheckLib
+{
+    public class OnCheckedEventArgs : EventArgs
+    {
+        public bool IsChecked { get; set; }
+
+        public OnCheckedEventArgs(bool isChecked)
+        {
+            IsChecked = isChecked;
+        }
+    }
+}
